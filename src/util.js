fb72c2d83b859e1ac6a4978e60c24e0865023fd3
Egi Gundari <egi.gundari@qraved.com>
2019-07-27 20:11:48 +0700

    add validation tests

--- src/util.js
+++ src/util.js
@@ -1,5 +1,11 @@
 module.exports = {
 
+    ERROR_MSG_INVALID_START_LAT_LON: "Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively",
+    ERROR_MSG_INVALID_END_LAT_LON: "End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively",
+    ERROR_MSG_INVALID_RIDER: "Rider name must be a non empty string",
+    ERROR_MSG_INVALID_DRIVER: "Driver name must be a non empty string",
+    ERROR_MSG_INVALID_VEHICLE: "Driver Vehicle must be a non empty string",
+
     /**
      *
      * @param startLatitude
@@ -11,29 +17,31 @@ module.exports = {
      * @param driverVehicle
      * @return {*} string
      */
-    validateCreateRideRequest(startLatitude, startLongitude, endLatitude, endLongitude, riderName, driverName, driverVehicle) {
+    validateCreateRideRequest(startLatitude, startLongitude, endLatitude,
+        endLongitude, riderName, driverName, driverVehicle) {
 
-        if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
-            return "Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively";
+        if (startLatitude < -90 || startLatitude > 90 ||
+            startLongitude < -180 || startLongitude > 180) {
+            return this.ERROR_MSG_INVALID_START_LAT_LON;
         }
 
-        if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
-            return "End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively";
+        if (endLatitude < -90 || endLatitude > 90 ||
+            endLongitude < -180 || endLongitude > 180) {
+            return this.ERROR_MSG_INVALID_END_LAT_LON;
         }
 
         if (typeof riderName !== "string" || riderName.length < 1) {
-            return "Rider name must be a non empty string";
+            return this.ERROR_MSG_INVALID_RIDER;
         }
 
         if (typeof driverName !== "string" || driverName.length < 1) {
-            return "Rider name must be a non empty string";
+            return this.ERROR_MSG_INVALID_DRIVER;
         }
 
         if (typeof driverVehicle !== "string" || driverVehicle.length < 1) {
-            return "Rider name must be a non empty string";
+            return this.ERROR_MSG_INVALID_VEHICLE;
         }
 
         return "";
-
     }
 };
